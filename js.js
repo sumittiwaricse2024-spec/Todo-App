07be972a40ade2e05ebe7a631f36d6174de30d9e
Sumit_Tiwari <sumittiwaricse2024@gmail.com>
2026-03-26 00:27:17 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+let taskList=[{item:"Hello",onDate:"21-56-58"},
+    {item:"Sumit Tiwari",onDate:"21-56-58"}];
+displayTask();
+function addTask(){
+    let inputTask=document.querySelector('.action-input1');
+    let inputDate=document.querySelector('.action-input2');
+    let inputInfo=inputTask.value;
+    let inputDateInfo=inputDate.value;
+    taskList.push({item:inputInfo,onDate:inputDateInfo});
+    inputTask.value='';
+    inputDate.value='';
+    displayTask();
+}
+
+function displayTask(){
+    let displayTask=document.querySelector('.listed-task');
+    // displayTask.innerText='';
+    let newHTML='';
+    for(let i=0;i<taskList.length;i++){
+        // let item=taskList[i].item;
+        // let onDate=taskList[i].onDate;
+        let {item,onDate}=taskList[i];
+        newHTML+=`
+                    <span class="task-added">${item}</span>
+                    <span class="task-added">${onDate}</span>
+                    <button class="delete-button" onclick="taskList.splice(${i},1);
+                    displayTask();">Delete</button>
+        `;
+    }
+    displayTask.innerHTML=newHTML;
+}
